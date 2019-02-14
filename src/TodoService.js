cbe90e173092bb474abde4d0d2847a15c7aafee2
Brandon Richey <diamondgfx@gmail.com>
2019-02-13 22:15:52 -0500

    Chapter 6 complete

--- src/TodoService.js
+++ src/TodoService.js
@@ -7,7 +7,7 @@ const fetchTodos = async () => {
 const createTodo = async description => {
   const res = await fetch("/api/todos", {
     method: "POST",
-    headers: { accept: "application/json" },
+    headers: { accept: "application/json", "content-type": "application/json" },
     body: JSON.stringify({
       description: description,
       critical: false,
@@ -21,7 +21,7 @@ const createTodo = async description => {
 const deleteTodo = async todoId => {
   const res = await fetch(`/api/todos/${todoId}`, {
     method: "DELETE",
-    headers: { accept: "application/json" }
+    headers: { accept: "application/json", "content-type": "application/json" }
   });
   const json = await res.json();
   return { status: res.status, todos: json.todos };
